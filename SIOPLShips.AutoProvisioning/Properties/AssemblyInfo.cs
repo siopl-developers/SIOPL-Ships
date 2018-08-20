e5a8ca3bffcaddc1efb0d374fb1e1ab48aeff21f
Aditya Kresna <aditya.kresna@outlook.co.id>
2018-08-21 01:16:52 +0700

    Initial skeletons of projects

---
+++ SIOPLShips.AutoProvisioning/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SIOPLShips.AutoProvisioning")]
+[assembly: AssemblyDescription("Auto Provisioning scripts for Cloud Server, Edge Server, and Dredging Ship Operator Computers")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SIOPL Developers")]
+[assembly: AssemblyProduct("SIOPLShips.AutoProvisioning")]
+[assembly: AssemblyCopyright("Copyright © SIOPL Developers 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1d17bcf8-986d-41fc-a975-2fc6fdc49ccb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SIOPLShips.EdgeService.Windows/App.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIOPLShips.EdgeService.Windows
+{
+	static class App
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			ServiceBase[] ServicesToRun;
+			ServicesToRun = new ServiceBase[]
+			{
+				new WSInterface()
+			};
+			ServiceBase.Run(ServicesToRun);
+		}
+	}
+}
